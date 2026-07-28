78b8a0bb1885f82400a7d002bd545f26f7082981
Daniel Quah <danielquah148@yahoo.com>
2026-07-28 10:47:29 +1000

    Add new enums to package status tracking

---
+++ src/database/migrations/1785199087000-insert_package_status.sql
@@ -0,0 +1,2 @@
+
+INSERT into package_status (status, enums) values ('Onboard for Delivery', 'ONBOARD_FOR_DELIVERY');
---
+++ src/database/migrations/1785199087000-insert_package_status.ts
@@ -0,0 +1,21 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+import { readFileSync } from 'fs';
+import { join } from 'path';
+
+
+
+export class InsertPackageStatus1785199087000 implements MigrationInterface {
+
+    name = 'InsertPackageStatus1785199087000';
+
+    private read(file: string): string {
+        return readFileSync(join(__dirname, file), 'utf8').trim();
+    }
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(this.read('1785199087000-insert_package_status.sql'));
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {}
+
+}
